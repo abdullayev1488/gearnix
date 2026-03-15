48a424c3e249e75049a0ac0210df07a4f747f1ac
Elman Abdullayev <elmanabdullayev001@gmail.com>
2026-03-15 07:58:37 +0400

    remove proxy

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,18 @@
-import { defineConfig, loadEnv } from "vite"
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
-
-  return {
-    server: {
-      port: 5555,
-      host: true,
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
+export default defineConfig({
+  server: {
+    port: 5555,
+    host: true,
+  },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  }
+  },
 })
