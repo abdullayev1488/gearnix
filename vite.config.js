0f5082fe1e162808b5be689427ab916a023f6a2d
Elman Abdullayev <elmanabdullayev001@gmail.com>
2026-03-18 00:06:22 +0400

    allowed host

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ export default defineConfig({
   server: {
     port: 9000,
     host: true,
+    allowedHosts:['gearnix.elmancode.site']
   },
+  
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
