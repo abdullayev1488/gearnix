a960161344f98a17d3f5d05574b107bc9c0558b8
Elman Abdullayev <elmanabdullayev001@gmail.com>
2026-03-17 23:30:19 +0400

    change -port

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 5555,
+    port: 9000,
     host: true,
   },
   plugins: [react(), tailwindcss()],
